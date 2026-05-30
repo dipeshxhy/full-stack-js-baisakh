6b02279fbe3d43e5920195aaab0027e149d2b5a1
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-05-30 21:10:38 +0545

    fix issue

--- projects/ecommerce-site/vite.config.js
+++ projects/ecommerce-site/vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        shops: resolve(__dirname, 'shops.html'),
+        cart: resolve(__dirname, 'cart.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
   plugins: [tailwindcss()],
 });
