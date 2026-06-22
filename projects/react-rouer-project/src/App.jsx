21b619b510114347d47c0cb113194d4ba1e74b8e
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-22 08:03:54 +0545

    fix register link

--- projects/react-rouer-project/src/App.jsx
+++ projects/react-rouer-project/src/App.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import HeroPage from './pages/HeroPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -15,7 +15,7 @@ const App = () => {
         <Route path="job" element={<h1>Job Page</h1>} />
         <Route path="about" element={<h1>About Page</h1>} />
         <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<RegisterPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
       <Footer />
